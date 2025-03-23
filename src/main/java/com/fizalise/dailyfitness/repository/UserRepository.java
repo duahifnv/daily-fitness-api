e1e2aec3d488b24d1b0b6a262cdbbb27e5526a71
Максим Фоминцев <max135603@gmail.com>
2025-03-23 23:36:55 +0300

    Параметризован репозиторий

--- src/main/java/com/fizalise/dailyfitness/repository/UserRepository.java
+++ src/main/java/com/fizalise/dailyfitness/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.fizalise.dailyfitness.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    int PAGE_SIZE = 5;
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
