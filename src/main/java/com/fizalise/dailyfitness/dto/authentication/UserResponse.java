33befb1e8887f1ea79edb73d2b1336bd776f4098
Максим Фоминцев <max135603@gmail.com>
2025-03-25 20:48:54 +0300

    Примеры значений в дто

--- src/main/java/com/fizalise/dailyfitness/dto/authentication/UserResponse.java
+++ src/main/java/com/fizalise/dailyfitness/dto/authentication/UserResponse.java
@@ -11,17 +11,20 @@ public record UserResponse(
         @Schema(description = "Имя пользователя", example = "Юзер")
         String name,
         @Schema(description = "Почта", format = "email@domen.xx",
-                example = "mail@mail.org")
+                example = "user@mail.org")
         String email,
         @Schema(description = "Пол", enumAsRef = true)
         Gender gender,
-        @Schema(description = "Возраст", minimum = "18", maximum = "99")
+        @Schema(description = "Возраст", minimum = "18", maximum = "150",
+                example = "25")
         Integer age,
-        @Schema(description = "Вес", minimum = "0")
+        @Schema(description = "Вес", minimum = "10", maximum = "500",
+                example = "70")
         BigDecimal weight,
-        @Schema(description = "Рост", minimum = "0")
+        @Schema(description = "Рост", minimum = "50", maximum = "250",
+                example = "180")
         BigDecimal growth,
-        @Schema(description = "Рассчитанная дневная норма калорий")
+        @Schema(description = "Рассчитанная дневная норма калорий", example = "1800")
         Integer dailyNorm,
         @Schema(description = "Цель", enumAsRef = true)
         Goal goal,
